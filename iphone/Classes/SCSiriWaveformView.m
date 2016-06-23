0f53b84e450655b49fe84617926ef60a8c2aea4a
Dejan Levec <dejan@dejanlevec.com>
2016-06-23 09:58:59 +0200

    update iOS visualization (again)

--- iphone/Classes/SCSiriWaveformView.m
+++ iphone/Classes/SCSiriWaveformView.m
@@ -8,135 +8,139 @@
 
 #import "SCSiriWaveformView.h"
 
+static const CGFloat kDefaultFrequency          = 1.0f;
+static const CGFloat kDefaultAmplitude          = 5.0f;
+static const CGFloat kDefaultIdleAmplitude      = 0.07f;
+static const CGFloat kDefaultNumberOfWaves      = 3.0f;
+static const CGFloat kDefaultPhaseShift         = -0.1f;
+static const CGFloat kDefaultDensity            = 2.0f;
+static const CGFloat kDefaultPrimaryLineWidth   = 0.5f;
+static const CGFloat kDefaultSecondaryLineWidth = 0.5f;
+
 @interface SCSiriWaveformView ()
 
-@property (nonatomic) CAShapeLayer *pathLayer1;
-@property (nonatomic) CAShapeLayer *pathLayer2;
-@property (nonatomic) CAShapeLayer *pathLayer3;
+@property (nonatomic, assign) CGFloat phase;
+@property (nonatomic, assign) CGFloat amplitude;
+@property (nonatomic, assign) CGFloat idleAmplitude;
+@property (nonatomic, assign) CGFloat numberOfWaves;
+@property (nonatomic, assign) CGFloat density;
+@property (nonatomic, assign) CGFloat primaryWaveLineWidth;
+@property (nonatomic, assign) CGFloat secondaryWaveLineWidth;
+@property (nonatomic, strong) NSArray* frequencyForWaves;
+@property (nonatomic, strong) NSArray* phaseShiftForWaves;
+@property (strong, nonatomic) UIColor *waveFillColor;
+@property (strong, nonatomic) UIColor *waveStrokeColor;
 
 @end
 
-@implementation SCSiriWaveformView {
-    CGFloat random1, random2;
-}
+@implementation SCSiriWaveformView
 
-- (instancetype)initWithFrame:(CGRect)frame
-{
-	if (self = [super initWithFrame:frame]) {
-		[self setup];
-	}
-	
-	return self;
-}
-
-- (instancetype)initWithCoder:(NSCoder *)aDecoder
-{
-	if (self = [super initWithCoder:aDecoder]) {
-		[self setup];
-	}
-	
-	return self;
-}
-
-- (void) setWaveColor:(UIColor *) color
-{
-    _waveColor = color;
-    
-    UIColor *transparent = [color colorWithAlphaComponent:0.5];
-    
-    if (_pathLayer1 != nil) {
-        [_pathLayer1 setStrokeColor:color.CGColor];
-        [_pathLayer1 setFillColor:transparent.CGColor];
-        
-        [_pathLayer2 setStrokeColor:color.CGColor];
-        [_pathLayer2 setFillColor:transparent.CGColor];
-        
-        [_pathLayer3 setStrokeColor:[UIColor clearColor].CGColor];
-        [_pathLayer3 setFillColor:transparent.CGColor];
+-(instancetype)init {
+    if (self = [super init]) {
+        [self setup];
     }
+    return self;
 }
 
-- (void)setup
-{
-    random1 = (arc4random() % 100);
-    random2 = (arc4random() % 100);
-    
-	self.backgroundColor = [UIColor clearColor];
-	
-    UIBezierPath *path1 = [UIBezierPath new];
-    [path1 moveToPoint:CGPointMake(-50.0, 100.0)];
-    [path1 addCurveToPoint:CGPointMake(self.frame.size.width+100.0, 100.0) controlPoint1:CGPointMake(self.frame.size.width/2.0+50.0, -100.0) controlPoint2:CGPointMake(self.frame.size.width/2.0+50.0, 300.0)];
-    [path1 addLineToPoint:CGPointMake(self.frame.size.width+100.0, self.frame.size.height)];
-    [path1 addLineToPoint:CGPointMake(-50.0, self.frame.size.height)];
-    [path1 closePath];
-    
-    _pathLayer1 = [CAShapeLayer new];
-    [_pathLayer1 setPath:path1.CGPath];
-    [_pathLayer1 setLineWidth:2.0];
-    [_pathLayer1 setOpacity:0.5];
-    [self.layer addSublayer:_pathLayer1];
-    
-    UIBezierPath *path2 = [UIBezierPath new];
-    [path2 moveToPoint:CGPointMake(-50.0, 100.0)];
-    [path2 addCurveToPoint:CGPointMake(self.frame.size.width+100.0, 100.0) controlPoint1:CGPointMake(self.frame.size.width/2.0+50.0, -100.0) controlPoint2:CGPointMake(self.frame.size.width/2.0+50.0, 300.0)];
-    [path2 addLineToPoint:CGPointMake(self.frame.size.width+100.0, self.frame.size.height)];
-    [path2 addLineToPoint:CGPointMake(-50.0, self.frame.size.height)];
-    [path2 closePath];
-    
-    _pathLayer2 = [CAShapeLayer new];
-    [_pathLayer2 setPath:path2.CGPath];
-    [_pathLayer2 setLineWidth:1.0];
-    [_pathLayer2 setOpacity:0.5];
-    [self.layer addSublayer:_pathLayer2];
-    
-    UIBezierPath *path3 = [UIBezierPath new];
-    [path3 moveToPoint:CGPointMake(-50.0, 100.0)];
-    [path3 addCurveToPoint:CGPointMake(self.frame.size.width+100.0, 100.0) controlPoint1:CGPointMake(self.frame.size.width/2.0+50.0, -100.0) controlPoint2:CGPointMake(self.frame.size.width/2.0+50.0, 300.0)];
-    [path3 addLineToPoint:CGPointMake(self.frame.size.width+100.0, self.frame.size.height)];
-    [path3 addLineToPoint:CGPointMake(-50.0, self.frame.size.height)];
-    [path3 closePath];
-    
-    _pathLayer3 = [CAShapeLayer new];
-    [_pathLayer3 setPath:path3.CGPath];
-    [_pathLayer3 setOpacity:0.5];
-    [self.layer addSublayer:_pathLayer3];
-    
-    [self.layer setMasksToBounds:YES];
-    
-    self.waveColor = [UIColor blackColor];
+-(instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self setup];
+    }
+    return self;
 }
 
-- (void)updateWithLevel:(CGFloat)level
-{
-    CGFloat normalizedValue = level  * 100.0;
+-(instancetype)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super initWithCoder:aDecoder]) {
+        [self setup];
+    }
+    return self;
+}
+
+-(void)setup {
+    self.waveColor = [UIColor colorWithRed:85.0/255.0 green:209.0/255.0 blue:196.0/255.0 alpha:1.0];
+    self.backgroundColor = [UIColor clearColor];
     
-    CGFloat y = self.frame.size.height/3.0*2.0;
+    self.frequencyForWaves = @[@(kDefaultFrequency),@(kDefaultFrequency*0.5),@(kDefaultFrequency*1.3)];
     
-    UIBezierPath *path1 = [UIBezierPath new];
-    [path1 moveToPoint:CGPointMake(0.0, 100.0)];
-    [path1 addCurveToPoint:CGPointMake(self.frame.size.width, 140.0) controlPoint1:CGPointMake(self.frame.size.width/2.0, y-normalizedValue*0.7) controlPoint2:CGPointMake(self.frame.size.width/2.0, y+normalizedValue*0.7)];
-    [path1 addLineToPoint:CGPointMake(self.frame.size.width, self.frame.size.height)];
-    [path1 addLineToPoint:CGPointMake(-50.0, self.frame.size.height)];
-    [path1 closePath];
-    [_pathLayer1 setPath:path1.CGPath];
+    self.amplitude = kDefaultAmplitude;
+    self.idleAmplitude = kDefaultIdleAmplitude;
     
+    self.numberOfWaves = kDefaultNumberOfWaves;
+    self.phaseShiftForWaves = @[@(kDefaultPhaseShift),@(kDefaultPhaseShift*0.8),@(kDefaultPhaseShift*1.3)];
+    self.density = kDefaultDensity;
     
-    UIBezierPath *path2 = [UIBezierPath new];
-    [path2 moveToPoint:CGPointMake(-25.0, 50.0)];
-    [path2 addCurveToPoint:CGPointMake(self.frame.size.width+50.0, 90.0) controlPoint1:CGPointMake(self.frame.size.width/2.0+25.0, y+normalizedValue+random1) controlPoint2:CGPointMake(self.frame.size.width/2.0+25.0, y-normalizedValue-random1)];
-    [path2 addLineToPoint:CGPointMake(self.frame.size.width+50.0, self.frame.size.height)];
-    [path2 addLineToPoint:CGPointMake(-50.0, self.frame.size.height)];
-    [path2 closePath];
-    [_pathLayer2 setPath:path2.CGPath];
+    self.primaryWaveLineWidth = kDefaultPrimaryLineWidth;
+    self.secondaryWaveLineWidth = kDefaultSecondaryLineWidth;
+}
+
+-(void)setWaveColor:(UIColor *)waveColor {
+    _waveColor = waveColor;
+    self.waveStrokeColor = waveColor;
+    self.waveFillColor = [waveColor colorWithAlphaComponent:0.3];
+}
+
+-(void)updateWithLevel:(CGFloat)level {
+    self.phase += kDefaultPhaseShift;
+    self.amplitude = fmax(level, self.idleAmplitude);
+    [self setNeedsDisplay];
+}
+
+-(CGFloat)normalizedPowerLevelFromDecibels:(CGFloat)decibels{
+    if (decibels < -60.0f || decibels == 0.0f) {
+        return 0.0f;
+    }
+    return powf((powf(10.0f, 0.05f * decibels) - powf(10.0f, 0.05f * -60.0f)) * (1.0f / (1.0f - powf(10.0f, 0.05f * -60.0f))), 1.0f / 2.0f);
+}
+
+-(void)drawRect:(CGRect)rect {
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextClearRect(context, self.bounds);
     
+    [self.backgroundColor set];
+    CGContextFillRect(context, rect);
     
-    UIBezierPath *path3 = [UIBezierPath new];
-    [path3 moveToPoint:CGPointMake(0.0, 170.0)];
-    [path3 addCurveToPoint:CGPointMake(self.frame.size.width, 210.0) controlPoint1:CGPointMake(self.frame.size.width/2.0, y+normalizedValue-random2) controlPoint2:CGPointMake(self.frame.size.width/2.0, y+normalizedValue-random2)];
-    [path3 addLineToPoint:CGPointMake(self.frame.size.width+100.0, self.frame.size.height)];
-    [path3 addLineToPoint:CGPointMake(-50.0, self.frame.size.height)];
-    [path3 closePath];
-    [_pathLayer3 setPath:path3.CGPath];
+    // We draw multiple sinus waves, with equal phases but altered amplitudes, multiplied by a parable function.
+    for (int i = 0; i < self.numberOfWaves; i++) {
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        
+        CGContextSetLineWidth(context, (i == 0 ? self.primaryWaveLineWidth : self.secondaryWaveLineWidth));
+        
+        CGFloat height = CGRectGetHeight(self.bounds);
+        CGFloat halfHeight = height / 2.0f + 30.0*i;
+        CGFloat width = CGRectGetWidth(self.bounds);
+        CGFloat mid = width / 2.0f;
+        
+        const CGFloat maxAmplitude = halfHeight - 4.0f; // 4 corresponds to twice the stroke width
+        
+        
+        CGFloat progress = 0.9 - ((CGFloat)i)/self.numberOfWaves;
+        CGFloat normedAmplitude = (1.5f * progress - 0.5f) * self.amplitude * 1.2;
+        
+        CGFloat multiplier = MIN(1.0, (progress / 3.0f * 2.0f) + (1.0f / 3.0f));
+        if (i == 1) {
+            [[UIColor clearColor] set];
+        }
+        else {
+            [[self.waveStrokeColor colorWithAlphaComponent:multiplier] set];
+        }
+        [[self.waveFillColor colorWithAlphaComponent:multiplier] setFill];
+        
+        for (CGFloat x = 0; x<width +6.0 + self.density; x += self.density) {
+            // We use a parable to scale the sinus wave, that has its peak in the middle of the view.
+            CGFloat scaling = -pow(1 / mid * (x - mid), 2) + 1;
+            
+            CGFloat y = scaling * maxAmplitude * normedAmplitude * sinf(2 * M_PI *(x / width) * [self.frequencyForWaves[i]floatValue] + self.phase) + halfHeight;
+            
+            if (x == 0) {
+                CGContextMoveToPoint(context, -3.0, y);
+            } else {
+                CGContextAddLineToPoint(context, x, y);
+            }
+        }
+        CGContextAddLineToPoint(context, width+3.0, height+3.0);
+        CGContextAddLineToPoint(context, -3.0, height+3.0);
+        CGContextDrawPath(context, kCGPathFillStroke);
+    }
 }
 
 @end
